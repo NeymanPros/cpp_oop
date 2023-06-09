649e2cf476d12ec99a99a8c0ec7e4edaa54a3e0d
Alexey <113046879+NeymanPros@users.noreply.github.com>
2023-06-09 13:46:55 +0300

    Add files via upload

---
+++ neyman_a_d/prj.test/arrayt_test.cpp
@@ -0,0 +1,37 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+#include <arrayt/arrayt.hpp>
+
+TEST_CASE("arraytest") {
+	int i = -4;
+	ArrayT <int> a(5, i);
+	ArrayT <double> err(6, -90.2);
+	CHECK(a[2] == i);
+
+	for (int t = 0; t < a.ssize(); t++) {
+		a[t] = t;
+	}
+
+	CHECK(a[4] == 4);
+
+	a.remove(1);
+
+	CHECK(a[1] == 2);
+
+	ptrdiff_t rs = 8;
+	a.resize(rs);
+
+	CHECK(a.ssize() == rs);
+
+	for (int t = 0; t < a.ssize(); t++) {
+		a[t] = t;
+	}
+
+	ptrdiff_t p = 5;
+	a.insert(p, 88);
+
+	CHECK(a[5] == 88);
+
+	CHECK(a[p-1] == 4);
+}
