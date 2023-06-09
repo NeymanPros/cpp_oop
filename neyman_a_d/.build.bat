168343e55d046ccfa646a55b70ec44b760c7d6de
Alexey <113046879+NeymanPros@users.noreply.github.com>
2023-06-09 13:42:20 +0300

    Delete neyman_a_d directory

--- neyman_a_d/.build.bat
+++
@@ -1,2 +0,0 @@
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build --config Debug
--- neyman_a_d/prj.test/doctest_test.cpp
+++
@@ -1,7 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
-#include <doctest/doctest.h>
-#include <rational/rational.hpp>
-
-TEST_CASE("Check") {
-} 
